426067670acbd6e3b9f51c1a50f25e4a101797b7
BarteXikora <sbartek50@gmail.com>
2023-02-16 08:47:14 +0100

    Add verification by IP functionality.

--- server/api/functions/verifyByAuthOrIP.js
+++ server/api/functions/verifyByAuthOrIP.js
@@ -1,5 +1,34 @@
-const verifyByAuthOrIP = (req, res, next) => {
-    next()
+const jwt = require('jsonwebtoken')
+const dbConnect = require('./dbConnect')
+
+const checkForAuthToken = (token) => {
+    if (!token) return false
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET_TOKEN)
+        return verified.id
+
+    } catch (error) { return false }
+}
+
+const checkForIP = async (ip) => {
+    const answer = await dbConnect.cnt('SELECT * FROM zgloszenia_config_ips WHERE ip = ?;', [ip])
+
+    if (!answer.success) return false
+
+    return answer.data === 1
+}
+
+const verifyByAuthOrIP = async (req, res, next) => {
+    const token = req.header('auth-token')
+
+    if (checkForAuthToken(token)) return next()
+    if (await checkForIP(req.ip)) return next()
+
+    res.json({
+        success: false,
+        message: 'Nie udało się zweryfikować uprawnień.'
+    })
 }
 
 module.exports = verifyByAuthOrIP
