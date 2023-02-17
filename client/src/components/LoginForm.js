e8fa1cf7d204836cb33213a929a3a4d1bc3a5c5e
BarteXikora <sbartek50@gmail.com>
2023-02-17 13:59:23 +0100

    Add show and hide password button in login page.

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -6,6 +6,8 @@ import useCssAnimate from '../functions/useCssAnimate'
 import ButtonLoading from './ButtonLoading'
 
 import logo from '../img/logo-dark.png'
+import iconView from '../img/icon-view.png'
+import iconHide from '../img/icon-unview.png'
 
 const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/
@@ -15,6 +17,7 @@ const LoginForm = ({ login, apiLink }) => {
     const [password, setPassword] = useState('')
     const [warning, setWarning] = useState('')
     const [errorCode, setErrorCode] = useState('')
+    const [showPassword, setShowPassword] = useState(false)
 
     const [isLoading, setIsLoading] = useState(false)
 
@@ -95,7 +98,7 @@ const LoginForm = ({ login, apiLink }) => {
     }, [email, password])
 
     return <div className="container-xy section-gradient d-flex align-items-center justify-content-center">
-        <div className="section-login section-white text-center p-5 pb-4">
+        <div className="section-login section-white text-center py-5 px-3 px-md-5 pb-4 mb-5 mb-md-0">
             <img src={logo} className='img-fluid' alt="Lazzoni Group" />
 
             <h2 className="font-subtitle fw-bold py-5 pb-3">Zaloguj się!</h2>
@@ -112,15 +115,27 @@ const LoginForm = ({ login, apiLink }) => {
                     ref={emailInput}
                 />
 
-                <input
-                    className="mb-3"
-                    name="password"
-                    type="password"
-                    placeholder="Hasło"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                    ref={passwordInput}
-                />
+                <div className="input-btn mb-3">
+                    <input
+                        className="mb-0"
+                        name="password"
+                        type={showPassword ? 'text' : 'password'}
+                        placeholder="Hasło"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        ref={passwordInput}
+                    />
+
+                    <button
+                        className={`${showPassword && 'active'} btn btn-icon-small`}
+                        type='button'
+                        onClick={() => {
+                            setShowPassword(!showPassword)
+                        }}
+                    >
+                        <img src={showPassword ? iconView : iconHide} alt="Pokaż / ukryj hasło" />
+                    </button>
+                </div>
 
                 <div ref={warningRef} className="transition fw-bold font-wrong">
                     {warning}
@@ -130,7 +145,7 @@ const LoginForm = ({ login, apiLink }) => {
                     }
                 </div>
 
-                <button type="submit" className="mt-2 btn btn-prim">
+                <button type="submit" className="mt-3 btn btn-prim btn-full-width">
                     {isLoading && <ButtonLoading />}
 
                     Zaloguj się
