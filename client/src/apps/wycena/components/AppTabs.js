294b50070e2bd0e1689f36414611d742ebc5f73c
BarteXikora <sbartek50@gmail.com>
2023-02-28 13:36:12 +0100

    Added title section under tab.

--- client/src/apps/wycena/components/AppTabs.js
+++ client/src/apps/wycena/components/AppTabs.js
@@ -6,21 +6,36 @@ import Tab from './Tab'
 const AppTabs = () => {
     const { appState, appDispatch } = useContext(AppContext)
 
-    return <div className="tabs-container d-flex align-items-center justify-content-between px-3">
-        <h1 className="font-big fw-bold m-0">Wycena głowic:</h1>
+    return <div className="sticky-top">
+        <div className="tabs-container d-flex align-items-center justify-content-between px-3">
+            <h1 className="font-big fw-bold m-0">Wycena głowic:</h1>
 
-        <div>
-            <Tab
-                title='Wyceń głowicę'
-                isActive={appState.showCalculator}
-                onClick={() => appDispatch({ type: 'CHANGE_TAB', payload: true })}
-            />
+            <div>
+                <Tab
+                    title='Wyceń głowicę'
+                    isActive={appState.showCalculator}
+                    onClick={() => appDispatch({ type: 'CHANGE_TAB', payload: true })}
+                />
 
-            <Tab
-                title='Zapisane wyceny'
-                isActive={!appState.showCalculator}
-                onClick={() => appDispatch({ type: 'CHANGE_TAB', payload: false })}
-            />
+                <Tab
+                    title='Zapisane wyceny'
+                    isActive={!appState.showCalculator}
+                    onClick={() => appDispatch({ type: 'CHANGE_TAB', payload: false })}
+                />
+            </div>
+        </div>
+
+        <div className="section-white pt-3">
+            <h1 className="font-subtitle fw-bold px-3">
+                {
+                    appState.showCalculator ?
+                        'Kalkulator ceny głowicy:'
+                        :
+                        'Zapisane wyceny:'
+                }
+            </h1>
+
+            <hr />
         </div>
     </div>
 }
