236672b3b577558b93a77888666d1d9ed99231de
BarteXikora <sbartek50@gmail.com>
2023-03-02 10:46:19 +0100

    Moved default calculator state in appstate to separate file.

--- client/src/apps/wycena/functions/defaultAppState.js
+++ client/src/apps/wycena/functions/defaultAppState.js
@@ -1,32 +1,10 @@
 import defaultConfig from './defaultConfig'
+import defaultCalculatorValues from './defaultCalculatorValues'
 
 const defaultAppReducer = {
     showCalculator: true,
     config: defaultConfig,
-    calculator: {
-        times: {
-            timeProject: 0,
-            timeMilling: 0,
-            timeLathe: 0,
-            timeMontage: 0
-        },
-        shipment: { id: -1 },
-        spindles: [
-            {
-                type: { id: -1 },
-                montage: { id: -1 },
-                cnt: 0
-            }
-        ],
-        body: {
-            length: 0,
-            flatBar: { id: -1 }
-        },
-        extra: {
-            cluth: { id: -1 },
-            backHead: false
-        }
-    }
+    calculator: defaultCalculatorValues
 }
 
 export default defaultAppReducer
