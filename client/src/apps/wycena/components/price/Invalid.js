401c2dd7a631af18519923eb8b202582d1ec6874
BarteXikora <sbartek50@gmail.com>
2023-03-02 12:59:10 +0100

    Fixed 'invalid form' info component apperance.

--- client/src/apps/wycena/components/price/Invalid.js
+++ client/src/apps/wycena/components/price/Invalid.js
@@ -2,7 +2,7 @@ import imgCheck from '../../../../img/pic-check.png'
 
 const Invalid = () => {
     return <div className="section-gray px-3 py-4 d-flex align-items-center">
-        <img src={imgCheck} className='mx-3' width={110} alt="Sprawdź formularz kalkulacyjny!" />
+        <img src={imgCheck} className='me-3' width={110} alt="Sprawdź formularz kalkulacyjny!" />
 
         <div className='mt-3'>
             <h2 className="font-subtitle fw-bold m-0">Formularz nie jest uzupełniony prawidłowo!</h2>
