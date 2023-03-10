f86424b18cddcdde95282083c87383cb330349d7
BarteXikora <sbartek50@gmail.com>
2023-03-10 14:33:23 +0100

    Moved price formating function to separate file.

--- client/src/apps/wycena/components/price/PricesTable.js
+++ client/src/apps/wycena/components/price/PricesTable.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import formatePrices from '../../functions/formatePrices'
 
 import TableBody from './TableBody'
 
@@ -7,32 +8,10 @@ const PricesTable = ({ prices, currency = 'PLN' }) => {
 
     const [formatedPrices, setFormatedPrices] = useState({ ...emptyFormatedPrices })
 
-    const doFormat = (number) => {
-        number = number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$& ').replace('.', ',')
-
-        return number
-    }
-
     useEffect(() => {
-        let newFormatedPrices = { ...emptyFormatedPrices }
-
         if (!prices.newCustomer) return
 
-        newFormatedPrices.newCustomer['price'] = doFormat(prices.newCustomer.price) + ' ' + currency
-        newFormatedPrices.newCustomer['maxDiscount'] = prices.newCustomer.maxDiscount + ' %'
-        newFormatedPrices.newCustomer['discountValue']
-            = doFormat(prices.newCustomer.price * (prices.newCustomer.maxDiscount / 100)) + ' ' + currency
-        newFormatedPrices.newCustomer['afterDiscount']
-            = doFormat(prices.newCustomer.price * (1 - (prices.newCustomer.maxDiscount / 100))) + ' ' + currency
-
-        newFormatedPrices.regularCustomer['price'] = doFormat(prices.regularCustomer.price) + currency
-        newFormatedPrices.regularCustomer['maxDiscount'] = prices.regularCustomer.maxDiscount + ' %'
-        newFormatedPrices.regularCustomer['discountValue']
-            = doFormat(prices.regularCustomer.price * (prices.regularCustomer.maxDiscount / 100)) + ' ' + currency
-        newFormatedPrices.regularCustomer['afterDiscount']
-            = doFormat(prices.regularCustomer.price * (1 - (prices.regularCustomer.maxDiscount / 100))) + ' ' + currency
-
-        setFormatedPrices(newFormatedPrices)
+        setFormatedPrices(formatePrices(prices, currency))
 
     }, [prices])
 
@@ -68,8 +47,3 @@ const PricesTable = ({ prices, currency = 'PLN' }) => {
 }
 
 export default PricesTable
-
-// price={formatedPrices.newCustomer.price}
-            // maxDiscount={formatedPrices.newCustomer.maxDiscount}
-            // discountValue={formatedPrices.newCustomer.discountValue}
-            // afterDiscount={formatedPrices.newCustomer.afterDiscount}
