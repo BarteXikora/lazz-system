c27926ab4d2e2a9b051f4af1d2dfcdaaa9204d85
BarteXikora <sbartek50@gmail.com>
2023-03-28 09:02:16 +0200

    Added privilages varification function to add contact api.

--- server/api/zgloszenia/postContact.js
+++ server/api/zgloszenia/postContact.js
@@ -1,7 +1,25 @@
 const dbConnect = require('../functions/dbConnect.js')
+const verifyAppPrivilages = require('../functions/verifyAppPrivilages.js')
 
 const postContact = async (req, res) => {
+    const user = req.user
     const data = req.body
+
+    if (user !== undefined) {
+        const privilage = await verifyAppPrivilages(req.user, 'add')
+
+        if (!privilage.success) return res.json({
+            success: false,
+            message: 'Nie udało się sprawdzić uprawnień!',
+            code: '@API/zgloszenia/post-contact/' + privilage.code
+        })
+
+        if (!privilage.data) return res.json({
+            success: false,
+            message: 'Nie posiadasz uprawnień do wykonania tej czynności.',
+            code: '@API/zgloszenia/post-contact#00'
+        })
+    }
 
     let query = `INSERT INTO zgloszenia_contacts VALUES('', NOW(), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
 
@@ -14,7 +32,7 @@ const postContact = async (req, res) => {
     if (!answer) return res.json({
         success: false,
         message: 'Nie udało się dodać kontaktu do listy.',
-        error: '@API/zgloszenia/post-contact#00'
+        error: '@API/zgloszenia/post-contact#01'
     })
 
     res.json({ success: true })
