497af555fad4a95fa1a8008aa71573a8bbd1b844
BarteXikora <sbartek50@gmail.com>
2023-03-28 09:09:45 +0200

    Added privilages verification to delegate contact api.

--- server/api/zgloszenia/delegateContact.js
+++ server/api/zgloszenia/delegateContact.js
@@ -1,7 +1,23 @@
 const dbConnect = require('../functions/dbConnect.js')
+const verifyAppPrivilages = require('../functions/verifyAppPrivilages.js')
 
 const delegateContact = async (req, res) => {
+    const user = req.user
     const { contactID, workerID } = req.body
+
+    const privilage = await verifyAppPrivilages(req.user, 'delegate')
+
+    if (!privilage) return res.json({
+        success: false,
+        message: 'Nie udało się sprawdzić uprawnień!',
+        code: '@API/zgloszenia/delegate-contact/' + privilage.code
+    })
+
+    if (!privilage.data) return res.json({
+        success: false,
+        message: 'Nie posiadasz uprawnień do wykonania tej czynności.',
+        code: '@API/zgloszenia/delegate-contact#00'
+    })
 
     const answer = await dbConnect.q(
         'UPDATE zgloszenia_contacts SET worker = ? WHERE id = ?;',
@@ -11,7 +27,7 @@ const delegateContact = async (req, res) => {
     if (!answer) return res.json({
         success: false,
         message: 'Nie udało się przekazać zgłoszenia!',
-        error: '@API/zgloszenia/delegate-contact#00'
+        error: '@API/zgloszenia/delegate-contact#01'
     })
 
     res.json({ success: true })
