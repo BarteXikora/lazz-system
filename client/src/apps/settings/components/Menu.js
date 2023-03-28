3db638a3264312c32b4d1fed3cebd74a715bdfb4
BarteXikora <sbartek50@gmail.com>
2023-03-28 11:25:33 +0200

    Styled menu component and added icons.

--- client/src/apps/settings/components/Menu.js
+++ client/src/apps/settings/components/Menu.js
@@ -1,32 +1,33 @@
 import { useLocation, useNavigate } from 'react-router-dom'
 
+import iconUser from '../../../img/icon-user-settings.png'
+
 const Menu = ({ apps }) => {
     const currentApp = useLocation().pathname.replace('/settings/', '')
     const navigation = useNavigate()
 
-    return <div className="column-menu section-gray p-3">
-        <h1 className="font-title fw-bold pe-5 mb-3">Ustawienia:</h1>
-
-        <hr className="mt-0 p-0" />
+    return <div className="column-menu section-gray-l">
+        <section className="section-gray">
+            <h1 className="font-title fw-bold p-3 pe-5 mb-3">Ustawienia:</h1>
+        </section>
 
         <button
-            className={`btn btn-tr ${currentApp === 'user' ? 'btn-active' : ''} btn-full-width text-start mb-4`}
+            className={`btn btn-tr ${currentApp === 'user' ? 'btn-selected' : ''} btn-icon-text btn-full-width text-start px-3 py-2 mb-4`}
             onClick={() => navigation('/settings')}
         >
+            <img src={iconUser} alt="" />
             Ustawienia użytkownika
         </button>
-
-        <hr className="mt-0 p-0" />
-
-        <h2 className="font-subtitle fw-bold">Ustawienia aplikacji: </h2>
 
         {
             apps.map(app => <>
                 {
                     app.id >= 0 && <button
-                        className={`btn btn-tr ${app.slug === currentApp ? 'btn-active' : ''} btn-full-width text-start`}
+                        className={`btn btn-tr ${app.slug === currentApp ? 'btn-selected' : ''} btn-icon-text btn-full-width text-start py-2 px-3`}
                         onClick={() => navigation('/settings/' + app.slug)}
                     >
+                        <img src={`/img/icon-app-${app.slug}-black.png`} />
+
                         {app.name}
                     </button>
                 }
