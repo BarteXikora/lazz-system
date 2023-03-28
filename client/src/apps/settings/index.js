df76bc8522d5489ec42833b688c4f1e22d1bb785
BarteXikora <sbartek50@gmail.com>
2023-03-28 09:34:09 +0200

    Created app router.

--- client/src/apps/settings/index.js
+++ client/src/apps/settings/index.js
@@ -1,7 +1,25 @@
-import WorkInProgress from '../../components/WorkInProgress'
+import { useContext } from 'react'
+import SystemContext from '../../functions/SystemContext'
+import { Routes, Route, Navigate } from 'react-router-dom'
 
-const App = () => {
-    return <WorkInProgress />
+const Index = () => {
+    const { systemState } = useContext(SystemContext)
+
+    return <Routes>
+        <Route path='/' element={<Navigate to='user' />} />
+
+        <Route path='user' element={'user settings'} />
+
+        {
+            systemState.appsList.map((app, n) => <Route
+                key={n}
+                path={app.slug}
+                element={`${app.name} settings`}
+            />)
+        }
+
+        <Route path='*' element={<Navigate to='user' />} />
+    </Routes>
 }
 
-export default App
+export default Index
