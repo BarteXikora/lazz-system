dafed029682ae86970838cda5dc2af7c214abde4
BarteXikora <sbartek50@gmail.com>
2023-03-28 10:06:05 +0200

    Moved router from index to App component.

---
+++ client/src/apps/settings/App.js
@@ -0,0 +1,28 @@
+import { useContext } from 'react'
+import SystemContext from '../../functions/SystemContext'
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+import UserSettings from './components/apps/UserSettings'
+import AppSection from './components/AppSection'
+
+const App = () => {
+    const { systemState } = useContext(SystemContext)
+
+    return <Routes>
+        <Route path='/' element={<Navigate to='user' />} />
+
+        <Route path='user' element={<UserSettings />} />
+
+        {
+            systemState.appsList.map((app, n) => <Route
+                key={n}
+                path={app.slug}
+                element={<AppSection slug={app.slug} />}
+            />)
+        }
+
+        <Route path='*' element={<Navigate to='user' />} />
+    </Routes>
+}
+
+export default App
--- client/src/apps/settings/index.js
+++ client/src/apps/settings/index.js
@@ -1,28 +1,7 @@
-import { useContext } from 'react'
-import SystemContext from '../../functions/SystemContext'
-import { Routes, Route, Navigate } from 'react-router-dom'
-
-import UserSettings from './components/apps/UserSettings'
-import AppSection from './components/AppSection'
+import App from './App'
 
 const Index = () => {
-    const { systemState } = useContext(SystemContext)
-
-    return <Routes>
-        <Route path='/' element={<Navigate to='user' />} />
-
-        <Route path='user' element={<UserSettings />} />
-
-        {
-            systemState.appsList.map((app, n) => <Route
-                key={n}
-                path={app.slug}
-                element={<AppSection slug={app.slug} />}
-            />)
-        }
-
-        <Route path='*' element={<Navigate to='user' />} />
-    </Routes>
+    return <App />
 }
 
 export default Index
