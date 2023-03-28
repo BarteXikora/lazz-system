ff7fab5c24a920e297fd534d29e25434aef82a74
BarteXikora <sbartek50@gmail.com>
2023-03-28 14:09:38 +0200

    Created change user data form in settings.

--- client/src/apps/settings/components/apps/UserSettings.js
+++ client/src/apps/settings/components/apps/UserSettings.js
@@ -1,8 +1,13 @@
 import UserInfo from './users-settings/UserInfo'
+import ChangeData from './users-settings/ChangeData'
 
 const UserSettings = () => {
     return <div className="row p-3 m-0">
         <UserInfo />
+
+        <hr className='my-5' />
+
+        <ChangeData />
     </div>
 }
 
