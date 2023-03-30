b849be022f074281dad4cc9aafce8bb5feb191b6
BarteXikora <sbartek50@gmail.com>
2023-03-30 15:06:56 +0200

    Created shipment section and prepared next step.

---
+++ client/src/apps/wycena/components/calculator/StepMaterial.js
@@ -0,0 +1,5 @@
+const StepMaterial = () => {
+    return 'material'
+}
+
+export default StepMaterial
--- client/src/apps/wycena/components/calculator/StepShipment.js
+++ client/src/apps/wycena/components/calculator/StepShipment.js
@@ -1,5 +1,44 @@
+import { useState } from 'react'
+import SelectInput from '../../../../components/SelectInput'
+
+import StepMaterial from './StepMaterial'
+
 const StepShipment = () => {
-    return 'shipment'
+    const [shipment, setShipment] = useState({ id: -1 })
+
+    return <>
+        <div className="row pt-4 pb-5 m-0">
+            <div className="col-12 px-3">
+                <h2 className="font-big fw-bold m-0 mt-2">Region wysyłki:</h2>
+            </div>
+
+            <div className="col-6 mt-3 px-3">
+                <SelectInput
+                    options={[{ id: 0, name: 'Polska' }, { id: 1, name: 'UE' }]}
+                    state={shipment}
+                    setState={setShipment}
+                />
+            </div>
+        </div>
+
+        {
+            shipment.id === -1 ?
+
+                <div className="row pb-5 m-0">
+                    <div className="col-6 px-3">
+                        <div className="info-box w-100">
+                            <h5 className="font-big fw-bold m-0">
+                                Należy wybrać region wysyłki, aby przejść dalej...
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+
+                :
+
+                <StepMaterial />
+        }
+    </>
 }
 
 export default StepShipment
