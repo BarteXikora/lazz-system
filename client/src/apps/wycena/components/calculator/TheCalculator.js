fd9531d31bf32396728d505f907c886a1dfd762a
BarteXikora <sbartek50@gmail.com>
2023-03-30 12:48:53 +0200

    Removed all sections from calculator form.
    Sections components moved info temporary _old_calculator directory, so they are available to easy and
    fast reference. _old_calculator is going to be deleted after new calculator form is ready.

--- client/src/apps/wycena/components/calculator/TheCalculator.js
+++ client/src/apps/wycena/components/calculator/TheCalculator.js
@@ -1,21 +1,5 @@
-import SectionTimes from './SectionTimes'
-import SectionShipment from './SectionShipment'
-import SectionSpindles from './SectionSpindles'
-import SectionBody from './SectionBody'
-import SectionExtra from './SectionExtra'
-
 const TheCalculator = () => {
     return <>
-        <SectionTimes />
-
-        <SectionShipment />
-
-        <SectionSpindles />
-
-        <SectionBody />
-
-        <SectionExtra />
-
         <div className="my-5 py-5"></div>
     </>
 }
