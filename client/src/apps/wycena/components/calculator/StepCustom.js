60689528cfb4d57c23cb586bfaa49abf3a07095f
BarteXikora <sbartek50@gmail.com>
2023-03-31 10:50:44 +0200

    Created custom head section.

--- client/src/apps/wycena/components/calculator/StepCustom.js
+++ client/src/apps/wycena/components/calculator/StepCustom.js
@@ -1,5 +1,19 @@
+import StepSendMail from "./StepSendMail"
+
 const StepCustom = () => {
-    return 'custom'
+    return <>
+        <div className="row section-gray pt-4 pb-4 m-0">
+            <div className="col-12 px-3">
+                <h2 className="font-big fw-bold m-0 mt-2">Głowice specjalne:</h2>
+
+                <p className="m-0">
+                    Głowice spacjalne muszą zostać wycenione przez dział głowic!
+                </p>
+            </div>
+        </div>
+
+        <StepSendMail />
+    </>
 }
 
 export default StepCustom
