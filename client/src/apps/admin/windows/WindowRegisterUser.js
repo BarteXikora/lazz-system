6c12f0a0e50aecac3d341a173163546ade7a15ea
BarteXikora <sbartek50@gmail.com>
2023-04-05 13:43:53 +0200

    Created second section - system admin.

--- client/src/apps/admin/windows/WindowRegisterUser.js
+++ client/src/apps/admin/windows/WindowRegisterUser.js
@@ -2,7 +2,7 @@ import { useState, useContext } from 'react'
 import WindowContext from '../../../functions/WindowContext'
 import _ from 'lodash'
 
-import iconShow from '../../../img/icon-view.png'
+import iconView from '../../../img/icon-view.png'
 import iconHide from '../../../img/icon-unview.png'
 
 const WindowRegisterUser = () => {
@@ -11,6 +11,10 @@ const WindowRegisterUser = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [createAdmin, setCreateAdmin] = useState(false)
+
+    const [showPassword, setShowPassword] = useState(false)
+    const [adminPassword, setAdminPassword] = useState('')
 
     const handleGeneratePassword = () => {
         const lettersCnt = 4, bigLettersCnt = 4, numbersCnt = 4, symbolsCnt = 3
@@ -76,17 +80,98 @@ const WindowRegisterUser = () => {
             </label>
         </div>
 
-        <div className="col-12 mb-5">
+        <div className="col-12 mb-4">
             <div className="info-box">
                 <b>Uwaga!</b> Należy skopiować hasło użytkownika, gdyż po utworzeniu konta nie będzie można
                 go odczytać.
             </div>
         </div>
 
-        <div className="col-12 mb-2">
-            Administrator systemu
+        <hr className='mt-2 mb-4' />
+
+        <div className="col-12 mb-4">
+            <div className="checkbox-container ms-1">
+                <label>
+                    Administrator systemu
+
+                    <input
+                        type="checkbox"
+                        checked={createAdmin}
+                        onChange={() => setCreateAdmin(!createAdmin)}
+                    />
+
+                    <div className="checkmark"></div>
+                </label>
+            </div>
         </div>
 
+        {
+            createAdmin ?
+
+                <div className="col-12 mb-4">
+                    <div className="warning-box">
+                        <p className="font-big fw-bold mb-2">
+                            Uwaga! Tworzenie konta z uprawnieniem "administrator systemu".
+                        </p>
+
+                        <p className='mb-2'>
+                            Konto z uprawnieniem "administrator systemu" posiada automatycznie
+                            dostep do wszystkich aplikacji. Posiada możliwość edycji wszystkich
+                            ustawień systemu, tworzenia, edycji i usuwania kont uzytkowników itd.
+                        </p>
+
+                        <p className='fw-bold m-0'>
+                            Administrator systemu ma możliwość pozbawienia Cię takiegoż uprawnienia,
+                            a nawet usunięcia Twojego konta!
+                        </p>
+                    </div>
+                </div>
+
+                :
+
+                <>hehe</>
+        }
+
+        <hr className='mt-2 mb-4' />
+
+        <div className="col-6">
+            <span className="fw-bold">
+                Podaj swoje hasło w celu weryfikacji:
+            </span>
+
+            <div className="input-btn mt-2 mb-3">
+                <input
+                    className="mb-0"
+                    name="password"
+                    type={showPassword ? 'text' : 'password'}
+                    placeholder="Hasło"
+                    value={adminPassword}
+                    onChange={e => setAdminPassword(e.target.value)}
+                    autocomplete="new-password"
+                />
+
+                <button
+                    className={`${showPassword && 'active'} btn btn-icon-small`}
+                    type='button'
+                    onClick={() => {
+                        setShowPassword(!showPassword)
+                    }}
+                >
+                    <img src={showPassword ? iconView : iconHide} alt="Pokaż / ukryj hasło" />
+                </button>
+            </div>
+        </div>
+
+        <div className="col-12 d-flex justify-content-end">
+            <button
+                className="btn btn-sec ms-2 mb-1"
+                onClick={() => systemDispatch({ type: 'CLOSE_WINDOW' })}
+            >
+                Anuluj
+            </button>
+
+            <button className="btn btn-prim ms-2 mb-1">Utwórz konto</button>
+        </div>
     </div>
 }
 
